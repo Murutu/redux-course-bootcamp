746f5bf6571c87791b53c9e22cfd82ac348a567e
Murutu <murutupeter@gmail.com>
2023-06-06 09:41:42 +0300

    chore: configured the store

--- rtk-counter-app/app.js
+++ rtk-counter-app/app.js
@@ -18,16 +18,24 @@ const incrementBy = createAction("INCREMENT_BY", (amount, user) => {
 
 
 Pass in the amount I want to use to increase the counter & the user adding the number to the counter
+
+createReducer(initialState, (builder) => {
+    builder.addCase(increment, (state) => { // inside our callback function we can make changes to the state
+        state.counter // we r directly mutating the state bcz of the immer package
+    })
+})
 */
 
-const { createAction, nanoid } = require("@reduxjs/toolkit");
+const { createAction, nanoid, createReducer, configureStore } = require("@reduxjs/toolkit");
+
+const logger = require("redux-logger").createLogger();
 
 // initialState
 const initialState = {
     counter: 0,
 };
 
-// Action creator/ action
+// Action creator/ action / createAction
 const increment = createAction("INCREMENT");
 const decrement = createAction("DECREMENT");
 const resetCounter = createAction("RESET");
@@ -43,4 +51,33 @@ const incrementBy = createAction("INCREMENT_BY", (amount, user) => {
     }
 })
 
-console.log(incrementBy(14, "Peter")); // takes in 2 args the amount & user
+// console.log(incrementBy(14, "Peter")); // takes in 2 args the amount & user
+
+// createReducer builder callback
+const counterSlice = createReducer(initialState, (builder) => {
+    builder.addCase(increment, (state) => {
+        state.counter += 1;
+    });
+    builder.addCase(decrement, (state) => {
+        state.counter -= 1;
+    });
+    builder.addCase(resetCounter, (state) => {
+        state.counter = 0;
+    });
+    builder.addCase(incrementBy, (state,action) => {
+        state.counter += action.payload.amount
+    })
+});
+
+// Store
+
+const store = configureStore({
+    reducer: counterSlice,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+// Dispatch action
+store.dispatch(increment());
+
+
+console.log(store.getState());
